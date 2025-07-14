b4a53999ef55c14d31018de505086d4111e84e42
barbrete <barbara.cobo@estudante.iftm.edu.br>
2025-07-14 13:06:48 -0200

    Arrumando Cors

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import userRoutes from './Routes/UserRoutes';
 import preventivaRoutes from './Routes/PreventivaRoutes';
 import fotoRoutes from './Routes/FotoRoutes';
 import authRoutes from './Routes/AuthRoute';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("API rodando!");
 })
-
-app.use("/images", express.static(path.join(__dirname,"..", "uploads")));
-console.log('Pasta de uploads:', path.join(__dirname, "..", "uploads"));
 
 app.use('/auth', authRoutes);
 app.use('/usuarios', userRoutes);
